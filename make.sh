28e130bf0ead9e225c34e106b04e83c271e6294c
jean-baptiste <jb@debian-jb>
2020-04-15 23:18:13 +0200

    details

--- make.sh
+++ make.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 yacc -d parser.y
-mv y.tab.h parser.h
+mv y.tab.h parser.y.h
 mv y.tab.c parser.y.c
 lex parser.l
 mv lex.yy.c parser.l.c
 sed -i 's/typedef int YYSTYPE;/typedef union { float x; int i; char * str; char c; } YYSTYPE;/' parser.y.c
-sed -i 's/typedef int YYSTYPE;/typedef union { float x; int i; char * str; char c; } YYSTYPE;/' parser.h
+sed -i 's/typedef int YYSTYPE;/typedef union { float x; int i; char * str; char c; } YYSTYPE;/' parser.y.h
 gcc -c symboltable.c
 gcc parser.y.c parser.l.c symboltable.o -ly -ll -o parser
 rm -f symboltable.o
