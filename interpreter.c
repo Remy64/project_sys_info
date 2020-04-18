4b5fd81ea011cff23344ffbc8c293295d7305850
jean-baptiste <jb@debian-jb>
2020-04-18 02:44:32 +0200

    interpreter ok except for jump instructions

--- interpreter.c
+++ interpreter.c
@@ -2,14 +2,75 @@
 #include <stdio.h>
 #include <string.h>
 
-void interpreteLine(char * buffer) {
-    char * delim = "\t";
-    char * iter = strtok(buffer, delim);
+#define VAR_TABLE_SIZE 300
 
-    while(iter != NULL) {
-        printf("%s\t", iter);
-        iter = strtok(NULL, delim);
+int vars[VAR_TABLE_SIZE];
+
+void printInstruction(int numArgs, const char * instruction, int args[]) {
+    switch(numArgs) {
+        case 0:
+            printf("%s\n", instruction);
+            break;
+        case 1:
+            printf("%s\t%d\n", instruction, args[0]);
+            break;
+        case 2:
+            printf("%s\t%d\t%d\n", instruction, args[0], args[1]);
+            break;
+        case 3:
+            printf("%s\t%d\t%d\t%d\n", instruction, args[0], args[1], args[2]);
     }
+}
+
+void executeInstruction(const char * instruction, int args[]) {
+    if (strcmp(instruction, "ADD") == 0) {
+        vars[args[0]] = vars[args[1]] + vars[args[2]];
+    } else if (strcmp(instruction, "SOU") == 0) {
+        vars[args[0]] = vars[args[1]] - vars[args[2]];
+    } else if (strcmp(instruction, "MUL") == 0) {
+        vars[args[0]] = vars[args[1]] * vars[args[2]];
+    } else if (strcmp(instruction, "DIV") == 0) {
+        vars[args[0]] = vars[args[1]] / vars[args[2]];
+    } else if (strcmp(instruction, "INF") == 0) {
+        vars[args[0]] = vars[args[1]] < vars[args[2]];
+    } else if (strcmp(instruction, "SUP") == 0) {
+        vars[args[0]] = vars[args[1]] > vars[args[2]];
+    } else if (strcmp(instruction, "EQU") == 0) {
+        vars[args[0]] = vars[args[1]] == vars[args[2]];
+    } else if (strcmp(instruction, "COP") == 0) {
+        vars[args[0]] = vars[args[1]];
+    } else if (strcmp(instruction, "AFC") == 0) {
+        vars[args[0]] = args[1];
+    } else if (strcmp(instruction, "JMF") == 0) {
+        //TODO
+    } else if (strcmp(instruction, "JMP") == 0) {
+        //TODO
+    } else if (strcmp(instruction, "PRI") == 0) {
+        printf("Print : \"%d\"\n", vars[args[0]]);
+    }
+}
+
+void interpreteLine(char * buffer) {
+    char * instruction = NULL;
+    int args[3];
+
+    char * iter = NULL;
+    int i = 0;
+    while((iter = strsep(&buffer, "\t")) != NULL && i < 4) {
+        int length = strlen(iter);
+        if(iter[length-1] == '\n') {
+            iter[length-1] = '\0';
+        }
+
+        if(i == 0) {
+            instruction = iter;
+        } else {
+            args[i-1] = (int) strtol(iter, (char **) NULL, 10);
+        }
+        i++;
+    }
+    printInstruction(i-1, instruction, args);
+    executeInstruction(instruction, args);
 }
 
 int main(int argc, char * argv[]) {
@@ -30,7 +91,8 @@ int main(int argc, char * argv[]) {
     char * buffer = malloc(bufferSize * sizeof(char));
     size_t read;
     while((read = getline(&buffer, &bufferSize, asmFile)) != -1) {
-        interpreteLine(buffer);
+        interpreteLine(buffer); //we will probably have to store lines in a "char **" before interpreting them to handle "if" statements
     }
     free(buffer);
+    return 0;
 }
--- test.c
+++ test.c
@@ -1,6 +1,6 @@
 int main() {
 	int a;
-	a = 5 + 7;
+	a = 5 + 10;
 	int b = 12;
 	a = -a + b + 1;
 	return a;
